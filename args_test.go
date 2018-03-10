dc49070b1ec6809fa31ae157831d090717816572
Thomas Fan <thomasjpfan@gmail.com>
2018-03-10 16:31:09 -0500

    RFC: Remove unused env

--- args_test.go
+++ args_test.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/stretchr/testify/suite"
 	"math/rand"
 	"os"
 	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
 type ArgsTestSuite struct {
@@ -27,17 +28,6 @@ func (s *ArgsTestSuite) Test_GetArgs_ReturnsDefaultValues() {
 	s.Equal(5, args.Interval)
 	s.Equal(1, args.Retry)
 	s.Equal(0, args.RetryInterval)
-}
-
-func (s *ArgsTestSuite) Test_GetArgs_ReturnsIntervalFromEnv() {
-	expected := rand.Int()
-	intervalOrig := os.Getenv("DF_INTERVAL")
-	defer func() { os.Setenv("DF_INTERVAL", intervalOrig) }()
-	os.Setenv("DF_INTERVAL", strconv.Itoa(expected))
-
-	args := getArgs()
-
-	s.Equal(expected, args.Interval)
 }
 
 func (s *ArgsTestSuite) Test_GetArgs_ReturnsRetryFromEnv() {
---
+++ docs/usage.md
@@ -0,0 +1,5 @@
+# Usage
+
+*Docker Flow Swarm Listener* exposes a API to query series and to send notifications.
+
+##
